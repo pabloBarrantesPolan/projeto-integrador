a3daebdd26522d826c51a89ce2ae6f37a7a394b9
pabloBarrantesPolan <pablobp.cc@gmail.com>
2019-06-17 16:19:10 -0300

    versao 2 script db

--- projeto_integrador.sql
+++ projeto_integrador.sql
@@ -1,4 +1,7 @@
 create database loja_virtual;
+
+use loja_virtual;
+
 create table planos(
 id_plano int primary key auto_increment unique not null,
 nome varchar(30),
@@ -6,7 +9,7 @@ valor decimal(2),
 periodo int
 );
 
-use loja_virtual;
+
 
 create table fornecedor(
 id int primary key auto_increment unique not null,
@@ -18,7 +21,9 @@ cep varchar(8),
 assinatura int,
 pontuacao decimal(2)
 );
+
 drop table cliente;
+
 create table cliente(
 id int primary key auto_increment unique not null,
 nome varchar(50),
@@ -30,9 +35,10 @@ pontuacao decimal(2)
 );
 
 create table servico(
+id int primary key auto_increment unique not null,
 valor decimal(2),
 id_cliente int,
-id_fonecedor int,
+id_fornecedor int,
 id_empresa int,
 descricao text
 );
@@ -40,6 +46,31 @@ descricao text
 create table produto(
 id int primary key auto_increment unique not null,
 tipo varchar(30),
+descricao text,
 tamanho enum('pp','p','m','g','gg','eg','34','36','38', '40','42','44','46','48','50','52'),
+quantidade tinyint,
+imagem varchar(100),
 valor decimal(2),
+id_cliente int,
+id_fornecedor int,
+id_empresa int
 );
+
+create table empresa(
+id int primary key auto_increment unique not null,
+razao_social varchar(50),
+nome_fantasia varchar(50) not null unique,
+email varchar(30) unique,
+telefone varchar(15),
+cnpj varchar(14) unique,
+cep varchar(8),
+assinatura int,
+pontuacao decimal(2)
+);
+
+alter table empresa
+ADD CONSTRAINT empresa_plano  foreign key (assinatura) references planos(id_plano);
+
+
+describe fornecedor;
+-- ALTER TABLE objeto ADD CONSTRAINT fk_pessoa FOREIGN KEY(id) REFERENCES pessoa (id);
